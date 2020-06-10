2bfd444ede284d76f09a83c99a0580bb487cb94a
Richard de Borja <richard.deborja@gmail.com>
2020-06-10 09:30:42 -0400

    added argument for include list

--- bin/create_gisaid_files.py
+++ bin/create_gisaid_files.py
@@ -12,11 +12,16 @@ parser = argparse.ArgumentParser(description='Create required GISAID files')
 parser.add_argument('-o', '--output', help='filename to write metadata to')
 parser.add_argument('-p', '--path', default=os.getcwd(), help='path to search')
 parser.add_argument('-m', '--meta', help='meta file')
+parser.add_argument('-i', '--include', help='a list of samples to be included',
+                    default=None)
 if len(sys.argv) == 1:
     parser.print_help(sys.stderr)
     sys.exit(1)
 args = parser.parse_args()
 
+include_list = []
+if args.include:
+    include_list = gisaid.import_sample_include_list(file=args.include)
 metadata = gisaid.import_uhtc_metadata(file=args.meta)
 file_o = open(args.output, 'w')
 file_o.write(gisaid.get_column_header())
@@ -30,9 +35,18 @@ for gisaid_sample in gisaid_samples:
             date = metadata[samplename]['collection_date']
         else:
             date = 'unknown'
+        # if the include list is not empty, filter those samples in the include
+        # list
         _sample_dict = gisaid.create_metadata_dictionary(consensus=gisaid_sample[samplename]['consensus'],
                                                          sample_name=samplename,
                                                          fasta_header=gisaid_sample[samplename]['fasta_header'],
                                                          date=date)
-        file_o.write(gisaid.create_metadata_string(_sample_dict))
-        file_o.write("\n")
+        if not include_list:
+            if samplename in include_list:
+                file_o.write(gisaid.create_metadata_string(_sample_dict))
+                file_o.write("\n")
+            else:
+                continue
+        else:
+            file_o.write(gisaid.create_metadata_string(_sample_dict))
+            file_o.write("\n")
