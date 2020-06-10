c4fe2e17baf7298f2931c0276a8ec32be8e6bf02
Richard de Borja <richard.deborja@gmail.com>
2020-06-10 14:43:06 -0400

    added include list fix

--- bin/create_gisaid_files.py
+++ bin/create_gisaid_files.py
@@ -12,6 +12,7 @@ parser = argparse.ArgumentParser(description='Create required GISAID files')
 parser.add_argument('-o', '--output', help='filename to write metadata to')
 parser.add_argument('-p', '--path', default=os.getcwd(), help='path to search')
 parser.add_argument('-m', '--meta', help='meta file')
+parser.add_argument('-q', '--qc', help='the summary QC file')
 parser.add_argument('-i', '--include', help='a list of samples to be included',
                     default=None)
 if len(sys.argv) == 1:
@@ -22,7 +23,12 @@ args = parser.parse_args()
 include_list = []
 if args.include:
     include_list = gisaid.import_sample_include_list(file=args.include)
+qc_dict =  {}
+qc_dict = gisaid.get_coverage_dictionary(file=args.qc)
 metadata = gisaid.import_uhtc_metadata(file=args.meta)
+
+# create the metadata file which will be imported into Excel for
+# GISAID upload
 file_o = open(args.output, 'w')
 file_o.write(gisaid.get_column_header())
 file_o.write("\n")
@@ -41,7 +47,7 @@ for gisaid_sample in gisaid_samples:
                                                          sample_name=samplename,
                                                          fasta_header=gisaid_sample[samplename]['fasta_header'],
                                                          date=date)
-        if not include_list:
+        if include_list:
             if samplename in include_list:
                 file_o.write(gisaid.create_metadata_string(_sample_dict))
                 file_o.write("\n")
