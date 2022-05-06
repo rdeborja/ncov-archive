1cbf17306f06a956d1786a42be57f2d5baea1433
Richard de Borja <rdeborja@oicr.on.ca>
2022-05-06 12:29:00 -0400

    code cleanup

--- bin/create_gisaid_files.py
+++ bin/create_gisaid_files.py
@@ -10,80 +10,99 @@ import yaml
 import datetime
 import ncov.archive.gisaid as gisaid
 
-parser = argparse.ArgumentParser(description='Create required GISAID files')
-parser.add_argument('-o', '--output', help='filename to write metadata to')
-parser.add_argument('-f', '--fasta', help='filename of output fasta')
-parser.add_argument('-p', '--path', default=os.getcwd(), help='path to search')
-parser.add_argument('-m', '--meta', help='meta file')
-parser.add_argument('-q', '--qc', help='the summary QC file')
-parser.add_argument('-c', '--config', help='YAML file containing GISAID run config')
-parser.add_argument('-i', '--include', help='a list of samples to be included',
-                    default=None)
-parser.add_argument('-e', '--exclude', help='a list of samples to remove',
-                    default=None)
-if len(sys.argv) == 1:
-    parser.print_help(sys.stderr)
-    sys.exit(1)
-args = parser.parse_args()
+def init_args():
+    parser = argparse.ArgumentParser(description='Create required GISAID files')
+    #parser.add_argument('-o', '--output', help='filename to write metadata to')
+    #parser.add_argument('-f', '--fasta', help='filename of output fasta')
+    parser.add_argument('-p', '--path', default=os.getcwd(), help='path to search')
+    parser.add_argument('-m', '--meta', help='meta file')
+    parser.add_argument('-q', '--qc', help='the summary QC file')
+    parser.add_argument('-c', '--config', help='YAML file containing GISAID run config')
+    parser.add_argument('-i', '--include', help='a list of samples to be included',
+                        default=None)
+    parser.add_argument('-e', '--exclude', help='a list of samples to remove',
+                        default=None)
+    if len(sys.argv) == 1:
+        parser.print_help(sys.stderr)
+        sys.exit(1)
+    return parser.parse_args()
 
-year = datetime.date.today().year
-config = dict()
-with open(args.config, 'r') as yaml_p:
-    config = yaml.full_load(yaml_p)
-
-include_list = list()
-if args.include:
-    include_list = gisaid.import_sample_include_list(file=args.include)
-exclude_list = list()
-if args.exclude:
-    exclude_list = gisaid.import_sample_exclude_list(file=args.exclude)
-
-qc_dict =  dict()
-qc_dict = gisaid.get_coverage_dictionary(file=args.qc)
-metadata = gisaid.import_uhtc_metadata(file=args.meta)
-multi_fasta_list = list()
-
-# create the metadata file which will be uploaded to GISAID
-file_o = open(args.output, 'w')
-file_o.write(gisaid.get_column_header(delimiter=','))
-file_o.write("\n")
-gisaid_samples = gisaid.get_consensus_fasta_files(path=args.path, year=year)
-
-for gisaid_sample in gisaid_samples:
-    for samplename in gisaid_sample:
-        if exclude_list:
-            if samplename in exclude_list:
-                print(' '.join(['Sample found in exclude list: ', samplename]))
-                continue
-        if samplename in metadata:
-            date = metadata[samplename]['collection_date']
-        else:
-            date = 'unknown'
-        # if the include list is not empty, filter those samples in the include
-        # list
-        _sample_dict = gisaid.create_metadata_dictionary(consensus=gisaid_sample[samplename]['consensus'],
-                                                         sample_name=samplename,
-                                                         fasta_header=gisaid_sample[samplename]['fasta_header'],
-                                                         coverage=qc_dict[samplename]['mean_depth'],
-                                                         fasta_file=args.fasta,
-                                                         date=date,
-                                                         config=config)
-        if include_list:
-            if samplename in include_list:
+def main():
+    args = init_args()
+    #year = datetime.date.today().year
+    config = dict()
+    with open(args.config, 'r') as yaml_p:
+        config = yaml.full_load(yaml_p)
+    submission_to = 'gisaid'
+    today = datetime.datetime.now()
+    outfile_prefix = f'{today.strftime("%Y%m%d")}_{config["sample_collected_by_short"]}_{submission_to}'
+    outfile_fasta = f'{outfile_prefix}.fasta'
+    outfile_meta = f'{outfile_prefix}.csv'
+    year = today.strftime("%Y")
+    
+    include_list = list()
+    if args.include:
+        include_list = gisaid.import_sample_include_list(file=args.include)
+    exclude_list = list()
+    if args.exclude:
+        exclude_list = gisaid.import_sample_exclude_list(file=args.exclude)
+    
+    qc_dict =  dict()
+    qc_dict = gisaid.get_coverage_dictionary(file=args.qc)
+    metadata = gisaid.import_uhtc_metadata(file=args.meta)
+    multi_fasta_list = list()
+    
+    # create the metadata file which will be uploaded to GISAID
+    #file_o = open(args.output, 'w')
+    file_o = open(outfile_meta, "w")
+    file_o.write(gisaid.get_column_header(delimiter=","))
+    file_o.write("\n")
+    gisaid_samples = gisaid.get_consensus_fasta_files(path=args.path, year=year)
+    
+    for gisaid_sample in gisaid_samples:
+        for samplename in gisaid_sample:
+            if exclude_list:
+                if samplename in exclude_list:
+                    print(' '.join(['Sample found in exclude list: ', samplename]))
+                    continue
+            if samplename in metadata:
+                date = metadata[samplename]['collection_date']
+            else:
+                date = 'unknown'
+            # if the include list is not empty, filter those samples in the include
+            # list
+            _sample_dict = gisaid.create_metadata_dictionary(consensus=gisaid_sample[samplename]['consensus'],
+                                                             sample_name=samplename,
+                                                             fasta_header=gisaid_sample[samplename]['fasta_header'],
+                                                             coverage=qc_dict[samplename]['mean_depth'],
+                                                             #fasta_file=args.fasta,
+                                                             fasta_file=outfile_fasta,
+                                                             date=date,
+                                                             config=config)
+            if include_list:
+                if samplename in include_list:
+                    multi_fasta_list.extend(gisaid.create_fasta_record(fasta=gisaid_sample[samplename]['consensus'],
+                                                                       header=gisaid_sample[samplename]['fasta_header']))
+                    file_o.write(gisaid.create_metadata_string(_sample_dict, delimiter=','))
+                    file_o.write("\n")
+                else:
+                    print(' '.join(['Excluding sample: ', samplename]))
+                    continue
+            else:
                 multi_fasta_list.extend(gisaid.create_fasta_record(fasta=gisaid_sample[samplename]['consensus'],
-                                                                   header=gisaid_sample[samplename]['fasta_header']))
+                                                                    header=gisaid_sample[samplename]['fasta_header']))
                 file_o.write(gisaid.create_metadata_string(_sample_dict, delimiter=','))
                 file_o.write("\n")
-            else:
-                print(' '.join(['Excluding sample: ', samplename]))
-                continue
-        else:
-            multi_fasta_list.extend(gisaid.create_fasta_record(fasta=gisaid_sample[samplename]['consensus'],
-                                                                header=gisaid_sample[samplename]['fasta_header']))
-            file_o.write(gisaid.create_metadata_string(_sample_dict, delimiter=','))
-            file_o.write("\n")
-# write the multi-sample FASTA list to a file
-with open(args.fasta, 'w') as fasta_out:
-    for line in multi_fasta_list:
-        fasta_out.write(line)
-        fasta_out.write("\n")
+    # write the multi-sample FASTA list to a file
+    #with open(args.fasta, 'w') as fasta_out:
+    with open(outfile_fasta, "w") as fasta_out:
+        for line in multi_fasta_list:
+            fasta_out.write(line)
+            fasta_out.write("\n")
+
+
+if __name__ == '__main__':
+    main()
+
+
+#__END__
